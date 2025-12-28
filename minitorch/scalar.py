cf9b629c4355cc1bacd11900021d99971e5fb2d4
Alex McIntosh <mcintalmo@example.com>
2025-12-27 23:15:29 -0600

    Task 1.3: Chain Rule

--- minitorch/scalar.py
+++ minitorch/scalar.py
@@ -164,8 +164,8 @@ class Scalar:
         assert h.last_fn is not None
         assert h.ctx is not None
 
-        # TODO: Implement for Task 1.3.
-        raise NotImplementedError("Need to implement for Task 1.3")
+        derivatives = h.last_fn._backward(h.ctx, d_output)
+        return zip(self.parents, derivatives, strict=True)
 
     def backward(self, d_output: float | None = None) -> None:
         """Calls autodiff to fill in the derivatives for the history of this object.
